b459375dac410c1d9574440ceeed48638d409b4f
Jacek Czastkiewicz <jacek.czastkiewicz@capgemini.com>
2015-06-15 16:47:29 +0200

    stuff

---
+++ wks/A_03_02_digital_input/digital_input_interrupt/digital_input_interrupt.ino
@@ -0,0 +1,20 @@
+#include <Arduino.h>
+volatile int isHigh = HIGH;
+void onOff() {
+  static unsigned long lastMillis = 0;
+  unsigned long newMillis = millis();
+  if (newMillis - lastMillis > 50) {
+    isHigh = !isHigh;
+    digitalWrite(13,isHigh);
+    lastMillis = newMillis;
+  }
+}
+void setup() {
+  pinMode(13, OUTPUT);
+  digitalWrite(13,isHigh);
+  attachInterrupt(0, onOff, FALLING);
+}
+void loop() {
+}
+
+
