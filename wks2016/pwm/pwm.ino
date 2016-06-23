c53be04967ee41ab4b161f29114ee88e37d3b29f
Jacek Czastkiewicz <jacek.czastkiewicz@capgemini.com>
2016-06-23 21:53:34 +0200

    more ppt and ino added

---
+++ wks2016/pwm/pwm.ino
@@ -0,0 +1,19 @@
+#include <Arduino.h> 
+void setup() {
+  pinMode(10, OUTPUT);
+  Serial.begin(9600);
+}
+void loop() {
+  char c = 0;
+  String s;
+  do {
+    if (Serial.available()) {
+      c = Serial.read();
+      s += c;      
+    }
+  } while(c != '\n');  
+  int v = s.toInt();
+  analogWrite(10, v);
+  Serial.println(v);
+}
+
